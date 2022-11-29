19a929f911aa9f49dd7a372386204d5ad33dd294
Ivan MSI <arigatory@gmail.com>
2022-11-29 22:58:48 +0300

    add grpc

---
+++ GoogleMapInfo/GoogleDistanceApi.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace GoogleMapInfo
+{
+    public class GoogleDistanceApi
+    {
+        private readonly IConfiguration _configuration;
+
+        public GoogleDistanceApi(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<GoogleDistanceData> GetMapDistance(string originCity, string destinationCity)
+        {
+            string? apiKey = _configuration["googleDistanceApi:apiKey"];
+
+            string? googleDistanceApiUrl = _configuration["googleDistanceApi:apiUrl"];
+
+            googleDistanceApiUrl += $"units=imperial&origins={originCity}&destinations={destinationCity}&key={apiKey}";
+            
+            using HttpClient client = new HttpClient();
+            HttpRequestMessage request = 
+                new HttpRequestMessage(HttpMethod.Get, new Uri(googleDistanceApiUrl)); 
+            
+            HttpResponseMessage response = await client.SendAsync(request); 
+            
+            response.EnsureSuccessStatusCode();
+            
+            await using Stream data = await response.Content.ReadAsStreamAsync();
+            
+            GoogleDistanceData? distanceInfo = 
+                await JsonSerializer.DeserializeAsync<GoogleDistanceData>(data);
+            
+            return distanceInfo;
+        }
+    }
+}
--- microservice-map-info/Program.cs
+++ microservice-map-info/Program.cs
@@ -1,3 +1,6 @@
+using GoogleMapInfo;
+using microservice_map_info.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddGrpc();
+builder.Services.AddTransient<GoogleDistanceApi>();
 
 var app = builder.Build();
 
@@ -17,9 +22,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<DistanceInfoService>();
+    endpoints.MapControllers();
+});
 
 app.Run();
